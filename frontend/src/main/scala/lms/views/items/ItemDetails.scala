5e1f69211089138126ef30b8ec2535d0f68edd10
clement.wee <clement.wee.hong.en@gmail.com>
2018-04-08 02:18:34 +0800

    Change Table headers for Item Info function

--- frontend/src/main/scala/lms/views/items/ItemDetails.scala
+++ frontend/src/main/scala/lms/views/items/ItemDetails.scala
@@ -12,7 +12,7 @@ object ItemDetails {
 class ItemDetails(items: ModelProperty[DataLoadingModel[LibraryItemInfo]]) {
   def render() = DataTable[LibraryItemInfo](
     items,
-    Seq("Series", "Title", "Series Order", "Number of Libraries With Stock"),
+    Seq("Series Title", "Item Title", "Series Order", "Number of Libraries With Stock"),
     (prop) => {
       val data = prop.get
       val orderStr = data.order.map(_.toString).getOrElse("")
