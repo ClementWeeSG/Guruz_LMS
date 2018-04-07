e44b3689ed7034e670bbb06f8751350e900f4e37
clement.wee <clement.wee.hong.en@gmail.com>
2018-04-08 01:56:19 +0800

    Update Item Popularity Backend

--- frontend/src/main/assets/api/index.php
+++ frontend/src/main/assets/api/index.php
@@ -79,11 +79,58 @@ FROM
 });
 
 Flight::route('GET /item-popularity', function(){
-	$start = $_GET["start"];
-	$end = $_GET["end"];
-	Flight::queryTable(function($conn) use ($start, $end){
-		//TODO
+	$start = Flight::request()->query->start;
+	$end = Flight::request()->query['end'];
+	$bet_clause = "";
+	$LEND = "Lend";
+	$RENEW = "Renew";
+	if($start){
+		if($end){ // both start and end are present
+			$bet_clause = ' and DATE(Tr_DateTime) between ? and ? ';
+		} else { // only start is present
+			$bet_clause = ' and DATE(Tr_DateTime)>=? ';
+		}
+	} else {
+		if($end){
+			$bet_clause = ' and DATE(Tr_DateTime)<=? '; // only end is present
+		} else {
+			$bet_clause = " "; // no parameters are provided
+		}
+	}
+	Flight::queryTable(function($conn) use ($start, $end, $bet_clause){
+		$q="SELECT temp.item AS itemID, i.Title AS itemTitle, temp.n1, temp.n2, temp.score, (@cnt := @cnt + 1) AS rank
+			FROM
+			(select t1.item, t1.n1 as n1, t2.n2 as n2, (t1.n1+t2.n2) as score
+			from (select it.Item_ID as item, count(*) as n1 from g1t06.transaction_item it, g1t06.transaction t
+			where it.Card_ID=t.Card_ID
+			and it.Tr_DateTime=t.Tr_DateTime".$bet_clause."
+			and t.Transaction_Type= 'Lend'
+			group by it.Item_ID)t1,
+			(select it.Item_ID as item, count(*) as n2 from g1t06.transaction_item it, g1t06.transaction t
+			where it.Card_ID=t.Card_ID
+			and it.Tr_DateTime=t.Tr_DateTime".$bet_clause."
+			and t.Transaction_Type= 'Renew'
+			group by it.Item_ID)t2
+			WHERE t1.item = t2.item
+			order by score DESC
+			limit 3) AS temp CROSS JOIN (SELECT @cnt := 0) AS temp2, item i
+			WHERE i.Item_ID=temp.item";
+		$stmt = mysqli_prepare($conn, $q);
+		if($start){
+			if($end){ // both start and end are present
+				mysqli_stmt_bind_param($stmt, 'ssss', $start,$end,$start, $end);
+			} else { // only start is present
+				mysqli_stmt_bind_param($stmt, 'ss', $start, $start);
+			}
+		} else {
+			if($end){ // only end is present
+				mysqli_stmt_bind_param($stmt, 'ss', $end,$end);
+			}
+	}
+	//mysqli_stmt_bind_param($stmt, 'ss', $LEND, $RENEW);
+		return $stmt;
 	});
+
 });
 
 Flight::route('GET /item-types', function(){
